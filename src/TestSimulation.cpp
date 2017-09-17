8673e0314fda1f96dd887f895c8eedf96f8f2084
James Ding <james.ding.illinois@gmail.com>
2017-09-17 03:29:25 -0500

    Project Helix: added test script

---
+++ src/TestSimulation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <random>
+#include <math.h>
+#include <chrono>
+#include <thread>
+#include <limits>
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Rand.h"
+#include "Simulation.h"
+
+using namespace std;
+int main(){
+   double mu = 0.0;
+   double sigma = 1.0;
+
+   cout<<"mu: "<<mu<<endl;
+   cout<<"sigma: "<<sigma<<endl;
+
+   return 0;
+}
